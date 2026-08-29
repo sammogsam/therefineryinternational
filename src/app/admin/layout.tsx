c5fc27d195c26fd379b88c5753b04d425c867674
Sanuel <sammogsam@gmail.com>
2026-08-29 08:51:07 +0100

    Fix admin dashboard mobile sidebar overlap layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -9,18 +9,20 @@ export default function AdminLayout({
   return (
     <div className="flex min-h-screen bg-gray-100">
 
-      {/* Sidebar */}
-      <Sidebar />
+      {/* Sidebar - Hidden on mobile, sticky sidebar on desktop */}
+      <div className="hidden md:block md:w-64 md:shrink-0">
+        <div className="fixed inset-y-0 z-30 w-64">
+          <Sidebar />
+        </div>
+      </div>
 
-      {/* Main Content */}
-      <div className="flex flex-1 flex-col">
-
+      {/* Main Content Area */}
+      <div className="flex flex-1 flex-col min-w-0">
         <Header />
 
-        <main className="flex-1 p-8">
+        <main className="flex-1 p-4 sm:p-6 md:p-8">
           {children}
         </main>
-
       </div>
 
     </div>
